a10434b6b9ed98702d923fa7629b99ac9040e0cc
rahulraheja49 <rraheja49@gmail.com>
2022-03-09 20:27:09 +0530

    remove github strategy

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const GitHubStrategy = require("passport-github2").Strategy;
+// const GitHubStrategy = require("passport-github2").Strategy;
 const chalk = require("chalk");
 const User = require("../models/User");
 
